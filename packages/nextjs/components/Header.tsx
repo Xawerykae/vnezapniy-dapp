707d41de1dad5885a111584e4ca838ee6555cb96
Xawerykae <mailformygames00@mail.ru>
2025-12-21 20:48:29 +0300

    Обновил футер: удалил неиспользуемые импорты

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -107,8 +107,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">SRE Challenges</span>
-            <span className="text-xs">Tokenization</span>
+            <span className="font-bold leading-tight">Joryan company</span>
+            <span className="text-xs">nft creator</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
